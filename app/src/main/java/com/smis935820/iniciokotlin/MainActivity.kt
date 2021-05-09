2bc97901318abcb9bc2dfd09cad2723aff389e6c
AmilcarLopezSMIS935820 <soft2320@gmail.com>
2021-05-09 14:38:45 -0600

    Revisión Final SMIS935820

--- app/src/main/java/com/smis935820/iniciokotlin/MainActivity.kt
+++ app/src/main/java/com/smis935820/iniciokotlin/MainActivity.kt
@@ -2,10 +2,25 @@ package com.smis935820.iniciokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn.setOnClickListener {
+            nombre()
+        }
+
+        btn2.setOnClickListener {
+            rst()
+        }
+    }
+
+    private fun nombre(){
+        nombre.text="Josué Amílcar López Benítez"
+    }
+
+    private fun rst(){
+        nombre.text="El contenido esta oculto"
     }
 }
